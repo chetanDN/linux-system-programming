0624b214d21b7316cb2ddadd9fec059d68f482c6
chetanDN <chetandevanaik@gmail.com>
2015-08-18 20:54:53 +0530

    Create 1.a.exeSeqUsingSyst.c
    program to execute Is, pwd & cal sequentially

---
+++ ProcessManagment/1.a.exeSeqUsingSyst.c
@@ -0,0 +1,16 @@
+/*
+Write a program to execute Is, pwd & cal sequentially
+a) using system( )
+
+Author	-	Chetan Naik
+mailId	-	chetandevanaik@gmail.com
+
+*/
+
+#include <stdio.h>
+main()
+{
+	system("ls");
+	system("pwd");
+	system("cal");
+}
