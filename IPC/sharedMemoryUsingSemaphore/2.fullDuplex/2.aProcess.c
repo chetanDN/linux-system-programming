7017b17b46c2d1f772ff6f3c1b7cd4aef2d3268a
chetanDN <chetandevanaik@gmail.com>
2015-08-19 05:44:12 +0530

    Create 2.aProcess.c
    Implement full duplex communication.
    Hint: use two shared memories.

---
+++ IPC/sharedMemoryUsingSemaphore/2.fullDuplex/2.aProcess.c
@@ -0,0 +1,82 @@
+/*
+Implement full duplex communication.
+Hint: use two shared memories.
+
+
+	Full-Duplex comunication using shared-memory and semaphore(much required here to synchronize)
+
+	5 sub-semaphore declared, 4 are used
+		two shared memory are got by shmp1 and shmp2
+
+	parent1(sem1) >> shmp1 >> child2(sem2)
+	child1(sem4)  >> shmp2 >> parent2(sem3)
+	
+Author	-	Chetan Naik
+mailId	-	chetandevanaik@gmail.com
+
+*/
+
+#include "header.h"
+main()
+{
+	char *shmp1,*shmp2;
+	int shmid,semid;
+	struct sembuf v;
+
+	shmid = shmget (4,50,IPC_CREAT|0644);
+	if(shmid<0)
+	{
+		perror("shmget");
+		return;
+	}
+
+	shmp1=shmat(shmid,NULL,0);		//get the allocated memory addr
+	bzero(shmp1,50);
+	shmp2=shmat(shmid,NULL,0);		//get the allocated memory addr
+	bzero(shmp2,50);
+	
+	semid=semget(4,5,IPC_CREAT|0644);	//creating 5 sub semaphores( but using last 4 only)
+	if(semid<0)
+	{
+		perror("semget");
+		return;
+	}
+
+	/* clearing the value or else use program to clear these subsemaphore*/
+	semctl(semid,1,SETVAL,0);
+	semctl(semid,2,SETVAL,1);
+	/*----------------------------------------------------*/
+
+	if(fork())
+	{
+		v.sem_num=1;
+		v.sem_op=0;
+		v.sem_flg=0;
+
+		printf("enter the data\n");
+		while(1)
+		{
+			semop(semid,&v,1);
+		 	semctl(semid,2,SETVAL,1);	//set lock to process 2 child
+			scanf("%s",shmp1);
+			semctl(semid,1,SETVAL,1);	//lock self
+			semctl(semid,2,SETVAL,0);	//open lock to process 2 chld
+		}
+	}
+	else
+	{
+		
+		v.sem_num=4;
+		v.sem_op=0;
+		v.sem_flg=0;
+
+		while(1)
+		{
+			semop(semid,&v,1);
+			semctl(semid,3,SETVAL,1);	//set lock to process 2 parent
+			printf("data p1 rx=%s\n",shmp2);
+			semctl(semid,4,SETVAL,1);	//self lock
+			semctl(semid,3,SETVAL,0);	//open lock for process 2 parent
+		}
+	}
+}
