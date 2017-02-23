37ed374d5f10b261d25437ae16e68dd0efef6487
chetanDN <chetandevanaik@gmail.com>
2017-02-23 23:39:04 +0530

    Update 01.Client.c

--- Client-Server-Programming/01.Prog/01.Client.c
+++ Client-Server-Programming/01.Prog/01.Client.c
@@ -29,48 +29,52 @@ int main(int argc,char **argv)
     
     if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
     {
-        printf("socket creation successful");
-        memset(&servaddr,0,sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_port = htons(SERVER_PORT);
-        
-        if((connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)))!=0)
-        {
-            printf("Error :  Server connect failed.\n");
-            exit(0);
-        }
-        printf("client got connected to the server\n");
-        printf("info Type 'exit' to end the session\n");
-        
-        while(1){
-            ts = time(NULL);
-            printf("[%Enter Message] : ",asctime(localtime(&ts)));
-            scanf("%s",sendline);
-            printf("...%s",sendline);
-            
-            if(sendline == "exit")
-                break;
-                
-            if((n=write(sockfd,sendline,sizeof(sendline)))<0)
-            {
-                printf("Error : Client message sending to serverf failed\n");
-                return 1;
-            }
-            printf("Info : Client Message asent to server successfully\n");
-            
-            if((n=read(sockfd,recvline,sizeof(recvline)))<0)
-            {
-                printf("Error : server reply messsage didnt received\n");
-                break;
-            }
-            
-            ts=time(NULL);
-            printf("[%s Server Message] : %s\n\n",asctime(localtime(&ts)),recvline);
-            
-        }
-        close(sockfd);
-        printf("client terminated the server session\n");
+    	printf("Problem in creating the socket");
+  		exit(0);
     }
+    printf("socket creation successful");
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+    servaddr.sin_port = htons(SERVER_PORT);
+        
+    if((connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)))<0)
+    {
+        printf("Error :  Server connect failed.\n");
+        exit(0);
+    }
+    printf("client got connected to the server\n");
+    printf("info Type 'exit' to end the session\n");
+    
+    while(1){
+        ts = time(NULL);
+        printf("[%s Enter Message] : ",asctime(localtime(&ts)));
+        gets(sendline);
+        printf("...%s",sendline);
+        
+        if(sendline == "exit")
+            break;
+            
+        if((n=write(sockfd,sendline,sizeof(sendline)))<0)
+        {
+            printf("Error : Client message sending to serverf failed\n");
+            return 1;
+        }
+        printf("Info : Client Message asent to server successfully\n");
+        
+        if((n=read(sockfd,recvline,sizeof(recvline)))<0)
+        {
+            printf("Error : server reply messsage didnt received\n");
+            break;
+        }
+        
+        ts=time(NULL);
+        printf("[%s Server Message] : %s\n\n",asctime(localtime(&ts)),recvline);
+        
+    }
+    close(sockfd);
+    printf("client terminated the server session\n");
+    
     
     
     return 0;
