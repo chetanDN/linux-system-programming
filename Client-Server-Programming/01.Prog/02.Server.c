d3b4f66fc09c27dfbfd623b396e2b7d1f612b379
chetanDN <chetandevanaik@gmail.com>
2017-03-04 07:46:05 +0530

    Update 02.Server.c

--- Client-Server-Programming/01.Prog/02.Server.c
+++ Client-Server-Programming/01.Prog/02.Server.c
@@ -1,4 +1,3 @@
-/**/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,72 +6,68 @@
 #include <time.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
 #define MAXLINE 4096
 #define SERVER_PORT 4000
 #define LISTENQ 5
-
 int main(int argc,char **argv)
 {
-    
-    int listenfd,connfd,n;
-    socklen_t clilen;
-    char buff[MAXLINE];
-    struct sockaddr_in cliaddr,servaddr;
-    //char sendline[MAXLINE],recvline[MAXLINE];
-    time_t ts;
-    
-    if((listenfd=socket(AF_INET,SOCK_STREAM,0))<0)
-    {
-        printf("server socket creation failed\n");
-        exit(0);
-    }
-    
-    printf("Server Socket created successfully\n");
-
-    memset(&servaddr,0,sizeof(servaddr));
-	servaddr.sin_family =AF_INET;
-    servaddr.sin_port        = htons(SERVER_PORT);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
- 
-    //servaddr.sin_addr.s_addr=htons(SERVER_PORT);
-    
-    if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
-    {
-        printf("Error : Server bind to port %d failed \n",SERVER_PORT);
-        exit(0);
-    }
-    printf("Server bound to port %d successfully\n",SERVER_PORT);
-    if(listen(listenfd,LISTENQ)<0)
-    {
-        printf("ERROR : server is not listening\n");
-        exit(0);
-    }
-    
-    printf("Server is listening  ..\n client requests are accepted\n\n");
-    
-    for(;;)
-    {
-        clilen=sizeof(cliaddr);
-        if((connfd = accept(listenfd,(struct sockaddr *)&cliaddr,&clilen))<0)
+        int listenfd,connfd,n;
+        socklen_t clilen;
+        char buff[MAXLINE];
+        struct sockaddr_in cliaddr,servaddr;
+        //char sendline[MAXLINE],recvline[MAXLINE];
+        time_t ts;
+        if((listenfd=socket(AF_INET,SOCK_STREAM,0))<0)
         {
-            printf("Error : Server didnt accept the client request \n");
-            exit (0);
+                printf("server socket creation failed\n");
+                exit(0);
         }
-        printf("Server accepted the accept() request from client\n");
-        while((n=read(connfd,buff,MAXLINE))<0)
+        printf("Server Socket created successfully listenfd=%d\n",listenfd);
+        memset(&servaddr,0,sizeof(servaddr));
+        servaddr.sin_family =AF_INET;
+        servaddr.sin_port = htons(SERVER_PORT);
+        //servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+        //servaddr.sin_addr.s_addr=htons(SERVER_PORT);
+        if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
         {
-            ts=time(NULL);
-            printf("%s client message : %s \n\n",asctime(localtime(&ts)),buff);
-            fflush (stdin);
+                printf("Error : Server bind to port %d failed \n",SERVER_PORT);
+                exit(0);
         }
-        if(n==0)
-            printf("Error :Client Disconnected\n");
-        else if(n==-1)
-            printf("Error :Server receive operation failed\n");
+        printf("Server bound to port %d successfully\n",SERVER_PORT);
+        if(listen(listenfd,LISTENQ)<0)
+        {
+                printf("ERROR : server is not listening\n");
+                exit(0);
         }
- 
-    close(connfd);
-    
-    return 0;
+        printf("Server is listening ..\n client requests are accepted\n\n");
+        for(;;)
+        {
+                clilen=sizeof(cliaddr);
+                if((connfd = accept(listenfd,(struct sockaddr *)&cliaddr,&clilen))<0)
+                {
+                        printf("Error : Server didnt accept the client request \n");
+                        exit (0);
+                }
+                printf("Server accepted the accept() request from client\n");
+                while((n=read(connfd,buff,MAXLINE))!=0)
+                {
+                        ts=time(NULL);
+                        printf("%s client message : %s \n\n",asctime(localtime(&ts)),buff);
+                        write(connfd,buff,MAXLINE);
+                //      fflush (stdin);
+                }
+                if(n==0)
+                        printf("Error :Client Disconnected\n");
+                else if(n==-1)
+                        printf("Error :Server receive operation failed\n");
+                        
+                close(connfd);
+          }
+     close(listenfd);
+     printf("Server stopped listening\n");
+     return 0;
 }
+ 
+
+ 
